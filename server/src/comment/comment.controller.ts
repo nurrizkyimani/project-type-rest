dd978a7bfc24f46089b61d11584846d5b0df1092
bocahrokok <indieunistudy@gmail.com>
2020-08-20 11:38:19 +0700

    comment service done, not test

--- server/src/comment/comment.controller.ts
+++ server/src/comment/comment.controller.ts
@@ -1,22 +1,26 @@
-import { Controller, Get, Post, Delete, Param, Logger } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Param, Logger, Body } from '@nestjs/common';
 import { CommentService } from './comment.service';
+import { UserBuilder, user } from 'firebase-functions/lib/providers/auth';
+import { User } from 'src/users/user.decorator';
+import { commentDTO } from './comment.dto';
+import { DataSnapshot } from 'firebase-functions/lib/providers/database';
 
 @Controller('comment')
 export class CommentController {
 	constructor(private commentService: CommentService) {}
 
-	@Get()
-	showAllComment() {
-		this.commentService.showAllCommentPerConfessId;
+	@Get('confess/:id')
+	showAllComment(@Param('id') confess: string) {
+		this.commentService.showAllCommentPerConfessId(confess);
 	}
 
-	@Post()
-	createComment() {
-		// this.commentService.createComment();
+	@Post('idea/id')
+	createComment(@Param('id') confess: string, @User('id') user: string, @Body() data: commentDTO) {
+		this.commentService.createComment(confess, user, data);
 	}
 
 	@Delete(':id')
-	deleteComment(@Param('id') id: string) {
-		Logger.log('to delete');
+	deleteComment(@Param('id') id: string, @User('id') user: string) {
+		this.commentService.deleteComment(id, user);
 	}
 }
