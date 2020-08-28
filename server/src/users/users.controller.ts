655ed54dc9282446d803f995418550410b764e61
bocahrokok <indieunistudy@gmail.com>
2020-08-28 17:36:42 +0700

    users firebase admin done

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Get, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get(':id')
+  showUserbyId(
+    @Param('id')
+    uid: string,
+  ) {
+    return this.usersService.whoAmI(uid);
+  }
+
+  @Post(':id')
+  showUserbyTest(
+    @Param('id')
+    username: string,
+  ) {
+    return `this is post ${username}`;
+  }
+}
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import * as admin from 'firebase-admin';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  async whoAmI(uid: string) {
+    await admin
+      .auth()
+      .getUser(uid)
+      .then(function(userRecord) {
+        // See the UserRecord reference doc for the contents of userRecord.
+
+        console.log('Successfully fetched user data:', userRecord.toJSON());
+
+        return userRecord.toJSON();
+      })
+      .catch(function(error) {
+        console.log('Error fetching user data:', error);
+      });
+  }
+}
