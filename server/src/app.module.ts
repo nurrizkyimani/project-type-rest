fe7bc45f5ecc8d742bb4a0ce140fd431b0d4666a
bocahrokok <indieunistudy@gmail.com>
2020-08-28 16:33:03 +0700

    config firebase admin

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -18,9 +18,11 @@ import { UsersModule } from './users/users.module';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
 import { UserEntity } from './users/users.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot(),
     UsersModule,
     ConfessModule,
--- server/src/main.ts
+++ server/src/main.ts
@@ -5,31 +5,35 @@ import { Logger } from '@nestjs/common';
 import { Connection, getConnection } from 'typeorm';
 import * as admin from 'firebase-admin';
 import { ServiceAccount } from 'firebase-admin';
-
-const serviceAccount = require('../serviceAccountKey.json');
+import { ConfigService } from '@nestjs/config';
 
 const port = process.env.PORT || 3000;
 
 async function bootstrap() {
-	admin.initializeApp({
-		// credential: admin.credential.cert({
-		// 	clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-		// 	privateKey: process.env.FIREBASE_PRIVATE_KEY,
-		// 	projectId: process.env.FIREBASE_PROJECT_ID
-		// }),
-		credential: admin.credential.cert(serviceAccount),
-		databaseURL: 'https://type-rest.firebaseio.com'
-	});
+  const app = await NestFactory.create(AppModule);
+  const configService: ConfigService = app.get(ConfigService);
 
-	const app = await NestFactory.create(AppModule);
+  const adminConfig: ServiceAccount = {
+    projectId: configService.get<string>('FIREBASE_PROJECT_ID'),
+    privateKey: configService
+      .get<string>('FIREBASE_PRIVATE_KEY')
+      .replace(/\\n/g, '\n'),
+    clientEmail: configService.get<string>('FIREBASE_CLIENT_EMAIL'),
+  };
 
-	app.enableCors();
-	await app.listen(port);
+  admin.initializeApp({
+    credential: admin.credential.cert(adminConfig),
+    // credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://type-rest.firebaseio.com',
+  });
 
-	const connection = getConnection();
-	const isConnected: boolean = connection.isConnected;
+  app.enableCors();
+  await app.listen(port);
 
-	Logger.log(`Server start on ${port}`, `PORT`);
-	Logger.log(`Database is connected ? ${isConnected}`, `DatabaseConnect`);
+  const connection = getConnection();
+  const isConnected: boolean = connection.isConnected;
+
+  Logger.log(`Server start on ${port}`, `PORT`);
+  Logger.log(`Database is connected ? ${isConnected}`, `DatabaseConnect`);
 }
 bootstrap();
