804eae60559b05ddcbc0b76dd2bbc789ac4041b7
bocahrokok <indieunistudy@gmail.com>
2020-09-05 15:24:30 +0700

    add auth guard that works

--- server/shared/auth.guard.ts
+++ server/shared/auth.guard.ts
@@ -1,40 +1,59 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Injectable, CanActivate, ExecutionContext, HttpException, HttpStatus } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import * as admin from 'firebase-admin';
+import { HttpsError } from 'firebase-functions/lib/providers/https';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 	async canActivate(context: ExecutionContext): Promise<boolean> {
 		const request = context.switchToHttp().getRequest();
 
-		if (request.headers.authorization) {
+		// console.log(request.headers);
+
+		if (!request.headers.authorization) {
 			return false;
 		}
 
-		console.log(request);
+		// console.log('this is request'
+		// console.log('this sis header auth ' + request.headers.authorization);
 
-		console.log(request.header.authorization);
+		const decoded = await this.validateToken(request.headers.authorization);
 
-		const decoded = this.validateToken(request.headers.authorization);
-		if (decoded) {
-			request.user = decoded;
+		console.log(`this is decode uid : ${decoded.uid}`);
+
+		if (decoded.status == true) {
+			request.user = decoded.uid;
 			return true;
 		}
-
-		// return true;
 
 		return false;
 	}
 
 	async validateToken(auth_token: string) {
 		try {
-			const decodedToken = await admin.auth().verifyIdToken(auth_token);
+			if (auth_token.split(' ')[0] !== 'Bearer') {
+				throw new HttpException('Invalid token', HttpStatus.FORBIDDEN);
+			}
+
+			const real_token = auth_token.split(' ')[1];
+
+			// console.log(`real token is  ${real_token}`);
+
+			const decodedToken = await admin.auth().verifyIdToken(real_token);
 			let uid = decodedToken.uid;
 			console.log(uid);
-			return uid;
+
+			return {
+				status: true,
+				uid: uid
+			};
 		} catch (error) {
 			console.log(error);
-			return error;
+
+			return {
+				status: false,
+				uid: error
+			};
 		}
 	}
 }
